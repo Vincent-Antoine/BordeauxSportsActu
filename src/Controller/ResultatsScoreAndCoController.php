6826edf403f3709ade63052c714d5280025ca7f2
Vincent-Antoine <vincentcomparato69@gmail.com>
2025-03-24 11:57:49 +0100

    Ajout du rugby amateur OK - Mais bug sur accueil

--- src/Controller/ResultatsScoreAndCoController.php
+++ src/Controller/ResultatsScoreAndCoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Service\ScorencoService;
+use App\Service\ResultatsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ResultatsScoreAndCoController extends AbstractController
 {
     #[Route('/resultats-score-and-co', name: 'scoreandco')]
-    public function index(ScorencoService $scorencoService): Response
+    public function index(ResultatsService $resultatsService): Response
     {
+        // Clubs principaux avec leurs IDs
         $clubList = [
             33519   => 'Basket Ambitions Girondines',
             108012  => 'JSA Basket',
@@ -21,10 +22,36 @@ class ResultatsScoreAndCoController extends AbstractController
             276898  => 'Les Boxers Hockey sur glace',
         ];
 
-        $results = $scorencoService->getMatchsForClubs($clubList);
+        $results = $resultatsService->getAllResults($clubList);
+
+        // Liste des clubs de rugby amateur à afficher dans le <select>
+        $rugbyClubs = [
+            "Club Municipal de Floirac",
+            "Entente SP Bruges Blanquefort",
+            "Stade Bordelais",
+            "Étoile SP Eysinaise",
+            "CA Lormont Hauts de Garonne",
+            "AS Merignac Rugby",
+            "Drop de Beton",
+            "Pessac Rugby",
+            "St Medard En Jalles RC",
+            "RC Gradignan",
+            "Rassemblement RCV LBR",
+            "RC Cadaujacais",
+            "Rugby Club Parempuyre",
+            "Rugby Club de La Pimpine",
+            "AS St Aubin de Medoc",
+            "Union Rugby Clubs XV Ambares",
+            "Rugby Club Martignas Illac",
+            "Cl At Bordeaux Begles Gironde",
+            "RC Cestadais",
+            "Entente Ambares et Saint Loubes",
+            "Leognan Rugby"
+        ];
 
         return $this->render('scoreandco/index.html.twig', [
             'results' => $results,
+            'rugbyClubs' => $rugbyClubs,
         ]);
     }
 }
