4f1a0a7b4745ea3f603403efd4dfe95cb12fbd87
Vincent-Antoine <vincentcomparato69@gmail.com>
2025-05-07 15:02:43 +0200

    modification du crud user correction bug

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,12 +5,14 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -35,12 +37,23 @@ class UserCrudController extends AbstractCrudController
             EmailField::new('email', 'Adresse e-mail'),
             ArrayField::new('roles', 'Rôles')
                 ->setHelp('Utilisez "ROLE_ADMIN" pour un administrateur ou "ROLE_USER" pour un utilisateur standard.'),
-            TextField::new('plainPassword', 'Mot de passe')
-                ->onlyOnForms()
-                ->setMapped(false)
-                ->setRequired($pageName === 'new')
-                ->setHelp('Laissez vide pour ne pas modifier le mot de passe.'),
+            // Pas de champ mot de passe ici, on l'ajoute plus bas via le formBuilder
         ];
+    }
+
+    public function createEntityFormBuilder(string $pageName, $entityInstance, array $entityProperties): FormBuilderInterface
+    {
+        $formBuilder = parent::createEntityFormBuilder($pageName, $entityInstance, $entityProperties);
+
+        // Ajout manuel d’un champ mot de passe non mappé
+        $formBuilder->add('plainPassword', PasswordType::class, [
+            'required' => $pageName === Crud::PAGE_NEW,
+            'mapped' => false,
+            'label' => 'Mot de passe',
+            'help' => 'Laissez vide pour ne pas modifier le mot de passe.',
+        ]);
+
+        return $formBuilder;
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
@@ -49,9 +62,7 @@ class UserCrudController extends AbstractCrudController
             return;
         }
 
-        $request = $this->requestStack->getCurrentRequest();
-        $plainPassword = $request->request->all()['User']['plainPassword'] ?? null;
-
+        $plainPassword = $this->getPlainPasswordFromRequest();
         if (!empty($plainPassword)) {
             $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $plainPassword);
             $entityInstance->setPassword($hashedPassword);
@@ -66,14 +77,20 @@ class UserCrudController extends AbstractCrudController
             return;
         }
 
-        $request = $this->requestStack->getCurrentRequest();
-        $plainPassword = $request->request->all()['User']['plainPassword'] ?? null;
-
+        $plainPassword = $this->getPlainPasswordFromRequest();
         if (!empty($plainPassword)) {
             $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $plainPassword);
             $entityInstance->setPassword($hashedPassword);
         }
 
         parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function getPlainPasswordFromRequest(): ?string
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $formData = $request->request->all();
+
+        return $formData['User']['plainPassword'] ?? null;
     }
 }
